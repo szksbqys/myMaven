dfc91b9db094946d0ff0914fcb5014ae721a1a5c
szksbqys <1325646989@qq.com>
2023-05-01 19:12:58 +0800

    第一次修改 提交到本地和远程

--- src/main/java/com/lw/demo/SpringDemoApplication.java
+++ src/main/java/com/lw/demo/SpringDemoApplication.java
@@ -10,5 +10,6 @@ public class SpringDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringDemoApplication.class,args);
         System.out.println("启动成功");
+        //第一次修改 提交到本地和远程
     }
 }
