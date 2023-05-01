a8708b9d55cb60bdb888f9a5cdfec2c9ed3abf3a
szksbqys <1325646989@qq.com>
2023-05-01 21:09:15 +0800

    提交到本地master仓库

---
+++ src/test/java/applicationTest/EmployeeServiceTest.java
@@ -0,0 +1,8 @@
+package applicationTest;
+
+/**
+ * author HP
+ * date 2023/5/1 21:07
+ */
+public class EmployeeServiceTest {
+}
---
+++ src/test/java/javaApiTest/StringTest.java
@@ -0,0 +1,8 @@
+package javaApiTest;
+
+/**
+ * author HP
+ * date 2023/5/1 21:07
+ */
+public class StringTest {
+}
